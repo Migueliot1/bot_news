bb4eaf526f43673d09eca5df497ea39debde7f35
Migueliot1 <migueliot@protonmail.com>
2022-03-19 00:59:55 +0300

    Add proper stop command.

--- bot_fun.py
+++ bot_fun.py
@@ -1,12 +1,15 @@
 # Bot's functionality is here
 
 # All the necessary packages
+from http.server import ThreadingHTTPServer
 from telegram.ext.updater import Updater
 from telegram.update import Update
 from telegram.ext.callbackcontext import CallbackContext
 from telegram.ext.commandhandler import CommandHandler
 from telegram.ext.messagehandler import MessageHandler
 from telegram.ext.filters import Filters
+import threading
+import os
 
 # Token getter function
 from hidden import get_token
@@ -30,10 +33,17 @@ def unknown_cmd(update: Update, context: CallbackContext):
     update.message.reply_text(
         "Sorry, not a valid command. Try /help to see all commands.")
 
+def shutdown():
+    updater.stop()
+    updater.is_idle = False
+
+def stop(update: Update, context: CallbackContext):
+    threading.Thread(target=shutdown).start()
 
 updater.dispatcher.add_handler(CommandHandler('start', start))
 updater.dispatcher.add_handler(CommandHandler('help', help))
-updater.dispatcher.add_handler(MessageHandler(Filters.text, unknown_cmd))
+updater.dispatcher.add_handler(CommandHandler('stop', stop))
+
 # Filter out unknown commands
 updater.dispatcher.add_handler(MessageHandler(Filters.command, unknown_cmd))
 # Filter out unknown messages
