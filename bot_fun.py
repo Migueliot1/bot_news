2d4af9982d56c39144bafaa7df2b98239ac74f3a
Migueliot1 <migueliot@protonmail.com>
2022-03-18 22:22:36 +0300

    Create bot_fun.py containing bot's functionality.
    Import all necessary packages in it.

---
+++ bot_fun.py
@@ -0,0 +1,8 @@
+# Bot's functionality is here
+
+from telegram.ext.updater import Updater
+from telegram.update import Update
+from telegram.ext.callbackcontext import CallbackContext
+from telegram.ext.commandhandler import CommandHandler
+from telegram.ext.messagehandler import MessageHandler
+from telegram.ext.filters import Filters
