5f5a9ede18be8eedd02404c74230e6e4f94a8e83
Migueliot1 <migueliot@protonmail.com>
2022-03-20 04:30:33 +0300

    Add a command which checks for news and one which posts them.

--- bot_fun.py
+++ bot_fun.py
@@ -10,6 +10,10 @@ from telegram.ext.filters import Filters
 import threading
 
 from hidden import get_token # Token getter function
+from news_re import get_data # Function which adds news articles to the table
+from extract import get_news # Function which retrieves a list full of data from the table
+
+arts = list() # List containing news articles
 
 # Implement hidden token
 updater = Updater(get_token(), use_context=True)
@@ -36,9 +40,25 @@ def shutdown():
 def stop(update: Update, context: CallbackContext):
     threading.Thread(target=shutdown).start()
 
+def check_for_news(update: Update, context: CallbackContext):
+    get_data()
+    update.message.reply_text(
+        "Look up succesful. Use /get to see if there are any news.")
+
+# not finished
+def post_arts(update: Update, context: CallbackContext):
+    arts = get_news()
+    if len(arts) == 0:
+        update.message.reply_text('No news *shrug*')
+    for i in range(len(arts)):
+        # send a message in chat
+        update.message.reply_text(arts[i])
+
 updater.dispatcher.add_handler(CommandHandler('start', start))
 updater.dispatcher.add_handler(CommandHandler('help', help))
 updater.dispatcher.add_handler(CommandHandler('stop', stop))
+updater.dispatcher.add_handler(CommandHandler('check', check_for_news))
+# updater.dispatcher.add_handler(CommandHandler('post', post_arts))
 
 # Filter out unknown commands
 updater.dispatcher.add_handler(MessageHandler(Filters.command, unknown_cmd))
